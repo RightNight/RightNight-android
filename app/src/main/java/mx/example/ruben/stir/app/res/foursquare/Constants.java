2653b2876afcadfa44255fbdf7c62bba0186294e
“rataton” <alfredo@umbralmedia.com>
2015-05-27 21:34:28 -0500

    Configuring options in venue details view

--- app/src/main/java/mx/example/ruben/stir/app/res/foursquare/Constants.java
+++ app/src/main/java/mx/example/ruben/stir/app/res/foursquare/Constants.java
@@ -45,6 +45,9 @@ public class Constants
     public static final String VENUE_FB = "fb";
     public static final String VENUE_TWITTER = "twitter";
 
+    // GNERAL STUFF
+    public static final String EMPTY_STRING = "---";
+
 
 
 
--- app/src/main/java/mx/example/ruben/stir/app/ui/fragments/ClubDetailsFragment.java
+++ app/src/main/java/mx/example/ruben/stir/app/ui/fragments/ClubDetailsFragment.java
@@ -1,13 +1,19 @@
 package mx.example.ruben.stir.app.ui.fragments;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.facebook.drawee.view.SimpleDraweeView;
+
+import java.util.Objects;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -34,8 +40,20 @@ public class ClubDetailsFragment extends android.support.v4.app.Fragment
     TextView venueCost;
     @InjectView(R.id.detaillDescription)
     TextView venueDetails;
-    @InjectView(R.id.detaillLink)
-    TextView venueLinks;
+    @InjectView(R.id.linkSite)
+    ImageView linkSite;
+    @InjectView(R.id.linkFB)
+    ImageView linkGB;
+    @InjectView(R.id.linkTwitter)
+    ImageView linkTwitter;
+
+    String mVenueHours;
+    String mVenueSite;
+    String mVenueLocation;
+    String mVenueCost;
+    String mVenueFacebook;
+    String mVenueTwitter;
+
 
     public ClubDetailsFragment() {}
 
@@ -66,17 +84,46 @@ public class ClubDetailsFragment extends android.support.v4.app.Fragment
     }
     private void initView()
     {
+
         clubImage.setImageURI(Uri.parse(getArguments().getString(Constants.CLUB_URL_IMAGE)));
         venueName.setText(getArguments().getString(Constants.CLUB_NAME));
-        venueHours.setText(getArguments().getString(Constants.VENUE_HOURS));
-        venueLocation.setText(getArguments().getString(Constants.VENUE_LOCATION));
-        venueCost.setText(getArguments().getString(Constants.VENUE_COST));
+
+
+        Log.i("OK ", "go link " + getArguments().getString(Constants.VENUE_LINK).trim());
+        mVenueHours = Objects.equals(getArguments().getString(Constants.VENUE_HOURS).trim(), "") ? Constants.EMPTY_STRING : getArguments().getString(Constants.VENUE_HOURS);
+        mVenueLocation = Objects.equals(getArguments().getString(Constants.VENUE_LOCATION).trim(), "") ? Constants.EMPTY_STRING : getArguments().getString(Constants.VENUE_LOCATION);
+        mVenueCost = Objects.equals(getArguments().getString(Constants.VENUE_COST).trim(), "") ? Constants.EMPTY_STRING : getArguments().getString(Constants.VENUE_COST);
+        mVenueSite = Objects.equals(getArguments().getString(Constants.VENUE_LINK).trim(), "") ? Constants.EMPTY_STRING : getArguments().getString(Constants.VENUE_LINK);
+        mVenueFacebook = Objects.equals(getArguments().getString(Constants.VENUE_FB).trim(), "") ? Constants.EMPTY_STRING : getArguments().getString(Constants.VENUE_FB);
+        mVenueTwitter = Objects.equals(getArguments().getString(Constants.VENUE_TWITTER).trim(), "") ? Constants.EMPTY_STRING : getArguments().getString(Constants.VENUE_TWITTER);
+
+        venueHours.setText(mVenueHours);
+        venueLocation.setText(mVenueLocation);
+        venueCost.setText(mVenueCost);
+        venueHours.setText(mVenueHours);
+
+        Log.i("OK ", "go link " + mVenueSite);
+        if (!Objects.equals(mVenueSite, Constants.EMPTY_STRING)){
+            linkSite.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                        Uri uLinkSite = Uri.parse(mVenueSite);
+                        Intent goSite = new Intent(Intent.ACTION_VIEW, uLinkSite);
+                        startActivity(goSite);
+                }
+            });
+        } else {
+            linkSite.setAlpha((float) .50);
+        }
+
+//        venueLocation.setText(getArguments().getString(Constants.VENUE_LOCATION));
+//        venueCost.setText(getArguments().getString(Constants.VENUE_COST));
 
         venueDetails.setText("Description");
 
-        venueLinks.setText(getArguments().getString(Constants.VENUE_LINK)+"\n"+
-                           getArguments().getString(Constants.VENUE_TWITTER)+"\n"+
-                           getArguments().getString(Constants.VENUE_FB));
+//        venueLinks.setText(getArguments().getString(Constants.VENUE_LINK)+"\n"+
+//                           getArguments().getString(Constants.VENUE_TWITTER)+"\n"+
+//                           getArguments().getString(Constants.VENUE_FB));
         //FALTA PHONE Y MAPS LOCATION ASI COMO ESTRELLAS
     }
 }
