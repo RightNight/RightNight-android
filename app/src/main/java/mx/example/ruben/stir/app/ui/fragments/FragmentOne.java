534d267e180e55f2758b61446624506d2762e326
Gober <reyesfragosoroberto@gmail.com>
2015-06-04 13:11:25 -0500

    Added the text for the principal Slider-Tutorial

--- app/src/main/java/mx/example/ruben/stir/app/ui/fragments/FragmentOne.java
+++ app/src/main/java/mx/example/ruben/stir/app/ui/fragments/FragmentOne.java
@@ -1,11 +1,13 @@
 package mx.example.ruben.stir.app.ui.fragments;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import mx.example.ruben.stir.R;
 
@@ -35,6 +37,10 @@ public class FragmentOne extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_one_layout, container, false);
+        TextView mtext = (TextView)rootView.findViewById(R.id.first_text);
+
+        Typeface font = Typeface.createFromAsset(getActivity().getAssets(),"fonts/Roboto-Thin.ttf");
+        mtext.setTypeface(font);
         return rootView;
     }
 
--- app/src/main/java/mx/example/ruben/stir/app/ui/fragments/FragmentThree.java
+++ app/src/main/java/mx/example/ruben/stir/app/ui/fragments/FragmentThree.java
@@ -1,11 +1,13 @@
 package mx.example.ruben.stir.app.ui.fragments;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import mx.example.ruben.stir.R;
 
@@ -36,6 +38,11 @@ public class FragmentThree extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_three_layout, container, false);
+
+        TextView mtext = (TextView)rootView.findViewById(R.id.third_text);
+
+        Typeface font = Typeface.createFromAsset(getActivity().getAssets(), "fonts/Roboto-Thin.ttf");
+        mtext.setTypeface(font);
         return rootView;
     }
 
